d3c93e7aba3a3ff8dc0ea0b246c1135cfd3a070f
SHIV SHARMA <152585900+MR-SHIV-SHARMA@users.noreply.github.com>
2025-05-18 14:29:06 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
         domains: [
-            'images.unsplash.com', 'aceternity.com', 'images.pexels.com'
+            'images.unsplash.com', 'aceternity.com', 'images.pexels.com', 'avatars.githubusercontent.com'
         ]
     }
 };
